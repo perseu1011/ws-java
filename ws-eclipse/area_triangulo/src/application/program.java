6224c09ca70281eb99527ecc79a3915a6c27aa95
perseu1011 <perseu.heric@gmail.com>
2022-01-25 20:25:03 -0300

    introducao de metodos e classes de objeto

--- ws-eclipse/area_triangulo/src/application/program.java
+++ ws-eclipse/area_triangulo/src/application/program.java
@@ -2,13 +2,41 @@ package application;
 
 import java.util.Scanner;
 
+import entities.Triangulo;
+
 public class program {
 
 	public static void main(String[] args) {
 		
 	Scanner sc = new Scanner(System.in);
 
+	Triangulo x , y;
 	
+	x = new Triangulo();
+	y = new Triangulo();
+	
+	System.out.println("entre com as medidas do triangulo X:");
+	x.a = sc.nextDouble();
+	x.b = sc.nextDouble();
+	x.c = sc.nextDouble();
+	System.out.println("entre com as medidas do triangulo Y:");
+	y.a = sc.nextDouble();
+	y.b = sc.nextDouble();
+	y.c = sc.nextDouble();
+	
+	
+	double areaX = x.area();
+	double areaY = y.area();
+	
+	System.out.printf("Area do triangulo X: %.4f%n", areaX);
+	System.out.printf("Area do triangulo X: %.4f%n", areaY);
+	
+	if (areaX > areaY) {
+		System.out.println("O triangulo X tem a maior area.");
+	}else {
+		System.out.println("O triangulo Y tem a maior area.");
+	}
+		
 	
 	sc.close();
 	}
